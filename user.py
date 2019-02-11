17dce6c070eb109cc837f4e7797d1e127b59adae
Anaissimpz <anaissimpenzwe@gmail.com>
2019-02-11 12:02:25 +0200

    initialise a method in user.py

--- user.py
+++ user.py
@@ -2,4 +2,11 @@ class USer:
     """
     Class that generates new instances of contacts
     """
-   
+    user_list=[]
+    
+    def __init__(self,first_name,last_name,username,password):
+        
+        self.first_name= first_name
+        self.last_name= last_name
+        self.username= username
+        self.password= password
